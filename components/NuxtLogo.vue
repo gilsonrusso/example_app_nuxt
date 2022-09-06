e6d24b7a2f2d405a8e1d35b2aa657acd368646db
Gilson Russo <gilsonrusso@outlook.com>
2022-09-05 20:31:15 -0400

    feat(all project): upload project

---
+++ components/NuxtLogo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    h1 Nuxt Logo
+  </div>
+</template>
+
+<style>
+.nuxt-logo {
+  height: 180px
+}
+</style>
---
+++ components/Tutorial.vue
@@ -0,0 +1,12 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    h1 Tutorial
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial',
+};
+</script>
